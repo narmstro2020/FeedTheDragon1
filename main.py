797e3029cc263f8258e509d5d8366a35af0a1756
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-09-12 08:17:52 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+#Set display surface
+pygame.init()
+
+#Set display surface
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 400
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Feed The Dragon")
+
+''' We will write more code here '''
+
+#The main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
